68aa05141b6239850933c1f361ba1710db84300b
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2024-03-14 20:23:30 -0300

    Adicionado outras Entities e feito Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,31 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Selers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Selers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Selers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.Data;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+
+
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            this.id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
