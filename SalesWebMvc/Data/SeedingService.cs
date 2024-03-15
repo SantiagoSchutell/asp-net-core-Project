f3d42bf1e0d0cc10ed064254e368b68bda5860bc
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2024-03-15 20:40:10 -0300

    Sellers Controller

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
-using System;
 
 namespace SalesWebMvc.Data
 {
     public class SeedingService
     {
-
         private SalesWebMvcContext _context;
 
         public SeedingService(SalesWebMvcContext context)
@@ -17,26 +18,24 @@ namespace SalesWebMvc.Data
 
         public void Seed()
         {
-            if (_context.Department.Any() || _context.Seller.Any() || _context.SellesRecord.Any())
-
-
-                {
-                return;
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SellesRecord.Any())
+            {
+                return; // DB has been seeded
             }
 
-
             Department d1 = new Department(1, "Computers");
-            Department d2 = new Department(1, "Eletronics");
-            Department d3 = new Department(1, "Fashion");
-            Department d4 = new Department(1, "Books");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
 
-
-            Seller s1 = new Seller(1, "Bob Brow", "bobdev@gmail.com", new DateTime(1998, 4, 21), 1800.0, d1);
-            Seller s2 = new Seller(1, "Maria Greeg", "mariasellers@gmail.com", new DateTime(1995, 2, 14), 1900.0, d2);
-            Seller s3 = new Seller(1, "Alex ASfas", "alex@gmail.com", new DateTime(1992, 7, 2), 2450.0, d3);
-            Seller s4 = new Seller(1, "Marta Reds", "martar@gmail.com", new DateTime(1999, 2, 5), 1800.0, d4);
-            Seller s5 = new Seller(1, "Donad Ssssso", "donad@gmail.com", new DateTime(1993, 4, 1), 1400.0, d2);
-            Seller s6 = new Seller(1, "Jizu Flederic", "jizuu@gmail.com", new DateTime(1997, 5, 12), 1800.0, d1);
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Maria Green", "maria@gmail.com", new DateTime(1979, 12, 31), 3500.0, d2);
+            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1988, 1, 15), 2200.0, d1);
+            Seller s4 = new Seller(4, "Martha Red", "martha@gmail.com", new DateTime(1993, 11, 30), 3000.0, d4);
+            Seller s5 = new Seller(5, "Donald Blue", "donald@gmail.com", new DateTime(2000, 1, 9), 4000.0, d3);
+            Seller s6 = new Seller(6, "Alex Pink", "bob@gmail.com", new DateTime(1997, 3, 4), 3000.0, d2);
 
             SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
             SalesRecord r2 = new SalesRecord(2, new DateTime(2018, 09, 4), 7000.0, SaleStatus.Billed, s5);
@@ -69,19 +68,17 @@ namespace SalesWebMvc.Data
             SalesRecord r29 = new SalesRecord(29, new DateTime(2018, 10, 23), 12000.0, SaleStatus.Billed, s5);
             SalesRecord r30 = new SalesRecord(30, new DateTime(2018, 10, 12), 5000.0, SaleStatus.Billed, s2);
 
-
             _context.Department.AddRange(d1, d2, d3, d4);
+
             _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
 
             _context.SellesRecord.AddRange(
-                 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
+                r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
-                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30);
+                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
+            );
 
             _context.SaveChanges();
         }
-
-
-
     }
 }
---
+++ SalesWebMvc/Views/Sellers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalesWebMvc.Views.Sellers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
