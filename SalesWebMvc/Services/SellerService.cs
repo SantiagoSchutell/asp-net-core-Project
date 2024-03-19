8eed730a4a3497eae7a1844f16b6655cb081587c
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2024-03-19 20:17:33 -0300

    pagina de vendedores

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+	public class SellerService
+	{
+
+		private readonly SalesWebMvcContext _context;
+
+		public SellerService(SalesWebMvcContext context)
+		{
+			_context = context;
+		}
+
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.ToList();
+		}
+
+
+
+	}
+}
