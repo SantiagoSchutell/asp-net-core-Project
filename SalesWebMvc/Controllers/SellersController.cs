87dd17c58a80f2e9cae12f92ad4d79d7b7c020c7
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2024-03-19 20:43:44 -0300

    Tela de cadastro de Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,25 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 
 namespace SalesWebMvc.Controllers
 {
-    public class SellersController : Controller
-    {
+	public class SellersController : Controller
+	{
 
-        private readonly SellerService _sellerService;
+		private readonly SellerService _sellerService;
 
-        public SellersController(SellerService sellerService)
-        {
-            _sellerService = sellerService;  
-        }
+		public SellersController(SellerService sellerService)
+		{
+			_sellerService = sellerService;
+		}
 
 
-        public IActionResult Index()
-        {
+		public IActionResult Index()
+		{
 
-            var list = _sellerService.FindAll();
-            return View(list);
-        }
-    }
+			var list = _sellerService.FindAll();
+			return View(list);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
